54e5cb0328a003da15aeffa77c4ee1f5f2e48228
mmispot <emiliadeboer1@gmail.com>
2025-03-20 09:52:01 +0100

    basic player movement

---
+++ Assets/Developers/Emilia/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveVec = new Vector3(horizontal, 0, vertical);
+        moveVec = moveVec.normalized;
+
+        controller.Move(moveVec * speed * Time.deltaTime);
+    }
+}
