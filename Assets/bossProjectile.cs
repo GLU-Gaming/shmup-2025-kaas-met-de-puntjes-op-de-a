48c11db499826f8c8d8d3e8fadf80dd5b6b6565d
NeokThijs <koen.abbink@ziggo.nl>
2025-03-28 14:19:20 +0100

    boss added

---
+++ Assets/bossProjectile.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class bossProjectile : MonoBehaviour
+{
+    private GameObject Player;
+    private PlayerController playerController;
+    private Rigidbody rb;
+    [SerializeField] private float moveSpeed = 10f;
+    private Vector3 leftBottom;
+
+    void Start()
+    {
+        leftBottom = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, 20f));                                                // haalt linker onderste hoek van het speelveld
+        Player = GameObject.FindWithTag("Player");
+        playerController = Player.GetComponent<PlayerController>();
+
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * moveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DespawsnOnExit();
+    }
+
+    public virtual void DespawsnOnExit()
+    {
+        if (transform.position.x <= leftBottom.x - 3)                                                                           // Als de transform van het object aan de linkerkant het scherm verlaat
+        {
+            Destroy(gameObject);                                                                                                // verwijder het object
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerController.Playerhealth -= 100;
+            Destroy(gameObject);
+        }
+    }
+}
+
