0fe7df2c0694d6670bdeb7aa6c72a6ff6d605690
NeokThijs <koen.abbink@ziggo.nl>
2025-04-09 09:11:21 +0200

    s

--- Assets/Developers/Koen/Scripts/EnvironmentMover.cs
+++ Assets/Developers/Koen/Scripts/EnvironmentMover.cs
@@ -4,19 +4,26 @@ public class EnvironmentMover : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private GameObject newTerrain;
-    private GameObject TerrainSpawn;
+    [SerializeField] private GameObject TerrainSpawn;
+    private bool SpawnedSuccessor;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.right * -1);
+        rb.AddForce(Vector3.right * -300);
+        SpawnedSuccessor = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rb.position.x < 208)
+        if (rb.position.x <= 230 && SpawnedSuccessor == false)
         {
             Instantiate(newTerrain, TerrainSpawn.transform.position, gameObject.transform.rotation);
+            SpawnedSuccessor = true;
+        }
+        else if(rb.position.x <= 180)
+        {
+            Destroy(gameObject);
         }
     }
 }
