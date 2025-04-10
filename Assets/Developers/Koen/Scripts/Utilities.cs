35bd50804351eee10fadf04743767a2631ac4628
mmispot <emiliadeboer1@gmail.com>
2025-04-10 14:20:15 +0200

    finalfinalgame

--- Assets/Developers/Koen/Scripts/Utilities.cs
+++ Assets/Developers/Koen/Scripts/Utilities.cs
@@ -1,25 +1,20 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
-using Unity.VisualScripting;
 public class Utilities : MonoBehaviour
 {
     [SerializeField] private GameObject Gamemanager;
     private GameBoss GameBoss;
+
     private void Start()
     {
         Gamemanager = GameObject.FindWithTag("GameManager");
-        GameBoss = Gamemanager.GetComponent<GameBoss>();
-    }
-    
+        //GameBoss = Gamemanager.GetComponent<GameBoss>();
 
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("storymode");
-        if (Gamemanager != null)
-        {
-            GameBoss.gameEnd = false;
-
-        }
+        GameBoss.gameEnd = false;
         Time.timeScale = 1;
     }
 
@@ -34,10 +29,8 @@ public class Utilities : MonoBehaviour
 
     public void ReturnToMain()
     {
-        if (Gamemanager != null)
-        {
-            GameBoss.gameEnd = false;
-        }
+        GameBoss.gameEnd = false;
+        //Time.timeScale = 1;
         SceneManager.LoadScene ("start");
     }
 
